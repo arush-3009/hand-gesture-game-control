c2b1a63fe1dd87041bb4f620a6ec32eec3da6a2f
Arush Handa <handaarush1237@gmail.com>
2025-10-15 22:11:10 -0400

    Create hand tracking file, create gesture file and 2 functions - to detect fist and to detect open hand, and create main file and include these to see that they work.

---
+++ main.py
@@ -0,0 +1,34 @@
+import mediapipe as mp
+import cv2
+
+from src import gestures, tracking
+
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise OSError("Cannot access Webcam.")
+
+detector = tracking.HandDetector()
+
+while cap.isOpened():
+
+    ret, frame = cap.read()
+    if not ret:
+        print('Webcam feed ended/disrupted.')
+        break
+
+    frame = cv2.flip(frame, 1)
+
+    frame = detector.find_hands(frame)
+    landmarks = detector.find_pos(frame)
+    if len(landmarks) != 0:
+        fist = gestures.is_fist(landmarks, display_output=True, img=frame)
+        open_hand = gestures.is_open(landmarks, display_output=True, img=frame)
+
+    cv2.imshow('Webcam', frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        print('Webcam feed ended. EXITING...')
+        break
+
+cap.release()
+cv2.destroyAllWindows()
